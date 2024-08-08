7f46f2edc55c6ab15e4cdc33c410088f4238caa5
acuervoa <kuerbo@gmail.com>
2024-08-08 15:08:22 +0200

    Add fugitive, gitsigns and comittia

--- lua/plugins/gitsigns.lua
+++ lua/plugins/gitsigns.lua
@@ -2,64 +2,62 @@ return {
 	"lewis6991/gitsigns.nvim",
 	dependencies = { "nvim-lua/plenary.nvim" },
 	config = function()
-		require("gitsigns").setup({
-			-- Configuration for signs
+		local gs = require("gitsigns")
+
+		gs.setup({
 			signs = {
-				add = { text = "│" }, -- Sign for added lines
-				change = { text = "│" }, -- Sign for changed lines
-				delete = { text = "_" }, -- Sign for deleted lines
-				topdelete = { text = "‾" }, -- Sign for top deleted lines
-				changedelete = { text = "~" }, -- Sign for changed and deleted lines
+				add = { text = "+" },
+				change = { text = "~" },
+				delete = { text = "_" },
+				topdelete = { text = "‾" },
+				changedelete = { text = "│" },
 			},
-			-- Git directory watch configuration
-			watch_gitdir = {
-				interval = 1000, -- Interval for git directory check in milliseconds
-				follow_files = true, -- Follow moved or renamed files
-			},
-			-- Blame line configuration
-			current_line_blame = true, -- Enable blame information for the current line
-			current_line_blame_opts = {
-				virt_text = true, -- Use virtual text for blame info
-				virt_text_pos = "eol", -- Position blame info at end of line
-				delay = 500, -- Delay in milliseconds before showing blame info
-			},
-			sign_priority = 6, -- Priority of git signs
-			update_debounce = 100, -- Debounce time for updates in milliseconds
-			status_formatter = nil, -- Use default status formatter
-			max_file_length = 40000, -- Max file length to process for git signs
-			-- Configuration for preview window
-			preview_config = {
-				border = "single", -- Border style for preview window
-				style = "minimal", -- Minimal style for preview window
-				relative = "cursor", -- Position preview window relative to cursor
-				row = 0, -- Row offset for preview window
-				col = 1, -- Column offset for preview window
-			},
+			word_diff = true,
+			on_attach = function(bufnr)
+				local function map(mode, l, r, opts)
+					opts = opts or {}
+					opts.buffer = bufnr
+					vim.keymap.set(mode, l, r, opts)
+				end
+
+				-- Navigation
+				map("n", "]c", function()
+					if vim.wo.diff then
+						return "]c"
+					end
+					vim.schedule(function()
+						gs.next_hunk()
+					end)
+					return "<Ignore>"
+				end, { expr = true, desc = "next hunk" })
+
+				map("n", "[c", function()
+					if vim.wo.diff then
+						return "[c"
+					end
+					vim.schedule(function()
+						gs.prev_hunk()
+					end)
+					return "<Ignore>"
+				end, { expr = true, desc = "previous hunk" })
+
+				-- Actions
+				map("n", "<leader>hp", gs.preview_hunk)
+				map("n", "<leader>hb", function()
+					gs.blame_line({ full = true })
+				end)
+			end,
 		})
 
-		-- Define highlights for deprecated settings
-		local hl_links = {
-			{ "GitSignsAdd", "GitGutterAdd" },
-			{ "GitSignsAddLn", "GitGutterAddLn" },
-			{ "GitSignsAddNr", "GitGutterAddNr" },
-			{ "GitSignsChange", "GitGutterChange" },
-			{ "GitSignsChangeLn", "GitGutterChangeLn" },
-			{ "GitSignsChangeNr", "GitGutterChangeNr" },
-			{ "GitSignsChangedelete", "GitGutterChange" },
-			{ "GitSignsChangedeleteLn", "GitGutterChangeLn" },
-			{ "GitSignsChangedeleteNr", "GitGutterChangeNr" },
-			{ "GitSignsDelete", "GitGutterDelete" },
-			{ "GitSignsDeleteLn", "GitGutterDeleteLn" },
-			{ "GitSignsDeleteNr", "GitGutterDeleteNr" },
-			{ "GitSignsTopdelete", "GitGutterDelete" },
-			{ "GitSignsTopdeleteLn", "GitGutterDeleteLn" },
-			{ "GitSignsTopdeleteNr", "GitGutterDeleteNr" },
-		}
-
-		-- Set highlights for git signs based on GitGutter highlights
-		for _, hl in ipairs(hl_links) do
-			vim.api.nvim_set_hl(0, hl[1], { link = hl[2] })
-		end
+		vim.api.nvim_create_autocmd("ColorScheme", {
+			pattern = "*",
+			callback = function()
+				vim.cmd([[
+      hi GitSignsChangeInline gui=reverse
+      hi GitSignsAddInline gui=reverse
+      hi GitSignsDeleteInline gui=reverse
+    ]])
+			end,
+		})
 	end,
 }
-
